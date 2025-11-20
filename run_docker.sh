05dfa5d0fb6b3d7b99698cc320ab816d8fe65426
Kilian Gerberding <ki-facebook@freenet.de>
2025-11-20 16:22:46 +0100

    remove wrong cp commands

--- run_docker.sh
+++ run_docker.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 docker run --shm-size=128g --rm -it \
-    --gpus device=1 \
+    --gpus device=0 \
     -v $PWD:/workspace \
     -v $PWD/data:/workspace/data \
     -v $PWD/segmentator:/workspace/segmentator \
     --name forestformer3d-container \
     forestformer3d-3dt \
-    bash -c "cp replace_mmdetection_files/loops.py /opt/conda/lib/python3.10/site-packages/mmengine/runner/ && \
+    bash -c "\
+    cp replace_mmdetection_files/loops.py /opt/conda/lib/python3.10/site-packages/mmengine/runner/ && \
     cp replace_mmdetection_files/base_model.py /opt/conda/lib/python3.10/site-packages/mmengine/model/base_model/ && \
     cp replace_mmdetection_files/transforms_3d.py /opt/conda/lib/python3.10/site-packages/mmdet3d/datasets/transforms/ && \
-    cp replace_mmdetection_files/loading.py /opt/conda/lib/python3.10/site-packages/mmdet3d/datasets/pipelines/loading.py && \
-    cp replace_mmdetection_files/forainetv2_dataset.py /opt/conda/lib/python3.10/site-packages/mmdet3d/datasets/forainetv2_dataset.py && \
-    python tools/test.py configs/oneformer3d_qs_radius16_qp300_2many.py work_dirs/model_file/epoch_3000_fix.pth"
+    python tools/test.py configs/oneformer3d_qs_radius16_qp300_2many.py model_file/epoch_3000_fix.pth"
